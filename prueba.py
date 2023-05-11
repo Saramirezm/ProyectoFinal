5179bbf2147a1db959759b56bfe92aa32e40c356
Saramirezm <125981440+Saramirezm@users.noreply.github.com>
2023-05-10 23:45:17 -0500

    comit

--- prueba.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import ast
-
-df = pd.read_csv('data.csv', encoding='utf-8')
-
-df['text_clean'] = df['text_clean'].apply(lambda x: ast.literal_eval(x))
-print(df.text_clean.head())
